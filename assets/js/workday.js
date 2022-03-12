205c2aaef526fc23aa5adfae3819789f11746923
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-03-12 18:08:17 -0500

    fixed time block columns

--- assets/js/workday.js
+++ assets/js/workday.js
@@ -1,4 +1,8 @@
 
+// display the date time, format from moment js library
+moment(Date);
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+
 
 let currentHour = (new Date()).getHours();
 const colorcode = document.getElementsByClassName("textarea");
@@ -13,10 +17,20 @@ $('textarea').each(function(){
     }
 });
 
-//save task to local storage:
-var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  };
+//save task to local storage when click on the save button
+
+    // let saveTasks = function() {
+    //     localStorage.setItem("tasks", JSON.stringify(tasks));
+    //   };
+
+
+$(".saveBtn").click(function() {    
+    let value = $(this).siblings("textarea").val();
+    localStorage.setItem('myVal', value);
+})
+
+const savedVal = localStorage.getItem('myVal')
+$(this).siblings("textarea").val(savedVal);
 
   
 
