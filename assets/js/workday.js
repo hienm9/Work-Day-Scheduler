379aead60b75a3917635cf9b439f70eadc6db430
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-03-11 23:02:15 -0500

    add timeblocks to html

---
+++ assets/js/workday.js
@@ -0,0 +1,15 @@
+let currentHour = (new Date()).getHours();
+const colorcode = document.getElementsByClassName("colorcode");
+$('.colorcode').each(function(){
+    let val = parseInt($(this).prop('id'));
+    if(val > currentHour && val < currentHour+6){
+        $(this).css('background-color','Blue');
+    }else if(val < currentHour && val > currentHour-6){
+        $(this).css('background-color','Red');
+    }else if(val === currentHour){
+        $(this).css('background-color','Green');
+    }else{
+        $(this).css('background-color','White');
+    }
+});
+
