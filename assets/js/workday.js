248d813a2997f80f9ef5f7c62bc64724a1b23562
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-03-12 13:27:12 -0500

    fixed the header style

--- assets/js/workday.js
+++ assets/js/workday.js
@@ -2,14 +2,32 @@ let currentHour = (new Date()).getHours();
 const colorcode = document.getElementsByClassName("colorcode");
 $('.colorcode').each(function(){
     let val = parseInt($(this).prop('id'));
-    if(val > currentHour && val < currentHour+6){
-        $(this).css('background-color','Blue');
-    }else if(val < currentHour && val > currentHour-6){
-        $(this).css('background-color','Red');
+    if(val < currentHour && val > currentHour-6){
+        $(this).css('background-color','Gray');
     }else if(val === currentHour){
-        $(this).css('background-color','Green');
+        $(this).css('background-color','Red');
     }else{
-        $(this).css('background-color','White');
+        $(this).css('background-color','Green');
     }
 });
 
+//save task to local storage:
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
+  
+
+// psuedo code
+
+// display all of the blocks 
+// display each bock with a business hour from 8  - 5
+// display the current time at the top
+
+// on the time blocks, display gray for time that is passed.
+// write a function that compare the block time to the current time
+    // then display correct color, gray for past, red for present, green for future
+
+// make the blocks clickable
+// function to allow user to enter an event on the block
+// save the information to local storage when click on the Save button
