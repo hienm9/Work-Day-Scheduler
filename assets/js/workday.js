832e7068aae6dc0b17a04ed15d1a9360e7841e66
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-03-13 00:53:43 -0500

    update the check time function for color display on the time block

--- assets/js/workday.js
+++ assets/js/workday.js
@@ -3,12 +3,31 @@ let currentDay = moment();
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 
 
-let currentHour = (new Date()).getHours();
-const colorcode = document.getElementsByClassName("textarea");
-$('textarea').each(function(){
+function checkTime() {
+    let currentHour = (new Date()).getHours();
+    console.log(currentHour);
 
+    const colorCode = document.getElementsByClassName("time-block");
+debugger;
 
-});
+    $(colorCode).each(function(){
+        let val = parseInt($(this).prop('id'));
+        if(val < currentHour){
+            $(this).css('background-color','Gray');
+        }else if(val === currentHour){
+            $(this).css('background-color','Red');
+        }else{
+            $(this).css('background-color','Green');
+        }
+    
+    });
+}
+checkTime();
+
+// add a class to the text area, depend on past, present, future.
+// display the code.
+// convert the time to 24 hrs format,
+// compare to the past, present, future
 
 //save task to local storage when click on the save button
 
